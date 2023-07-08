4fbd7b1793ed4297baba1132aa0a922893cb9916
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 00:11:45 +0700

    add type for data currencies

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -19,3 +19,42 @@ export type LogoProps = {
 export type ButtonProps = {
   onClick: () => void;
 };
+
+export type onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+export type WalletType = {
+  currencyGroup: string;
+  tokenSymbol: string;
+  decimal_point: number;
+  tokenType: string;
+  blockchain: string;
+  explorer: string;
+  listingDate: string;
+  blockchainName: string;
+  logo: string;
+};
+
+export type CurrencyType = {
+  currencyGroup: string;
+  color: string;
+  currencySymbol: string;
+  name: string;
+  logo: string;
+  decimal_point: number;
+  listingDate: string;
+  wallets: WalletType[];
+};
+
+export type PriceKey = 'pair' | 'latestPrice' | 'day' | 'week' | 'month' | 'year';
+
+export type PriceType = {
+  [key in PriceKey]: string;
+};
+
+export type CurrencyWithPriceType = CurrencyType & {
+  price: PriceType;
+};
+
+export type PricesType = {
+  [key: string]: PriceType;
+}
