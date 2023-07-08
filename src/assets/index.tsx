61f0ea28da5442197d2c3d3b11b9a60e46620c7d
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-08 23:52:34 +0700

    export assets from index file

---
+++ src/assets/index.tsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import burger from './burger.svg';
+import close from './close.svg';
+import flagIndo from './flagIndo.png';
+import search from './search.svg';
+
+const images = {
+  logo,
+  burger,
+  close,
+  flagIndo,
+  search
+};
+
+export default images;
