e1d662fa54ec34ebfbfeb6b035e9d6b8ce17e699
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 00:20:08 +0700

    create container search bar

--- src/components/Search/MobileSearch.tsx
+++ src/components/Search/MobileSearch.tsx
@@ -1,18 +1,31 @@
+import React from 'react';
+
 import images from '../../assets';
 
-const SearchIcon = () => (
-  <div className='cursor-pointer'>
+import { ButtonProps } from '../../constants/types';
+
+const SearchIcon = ({ onClick }: ButtonProps) => (
+  <div className='cursor-pointer' onClick={onClick}>
     <img src={images.search} />
   </div>
 );
 
 const MobileSearch = () => {
+  const [isHidden, setIsHidden] = React.useState<boolean>(true);
+
+  const onClickSearchIcon = () => {
+    setIsHidden(false);
+  };
+
   return (
     <div className='pt-8 relative md:hidden'>
       <div className='px-4 mb-6 flex justify-between items-center mx-auto'>
         <span className='font-bold text-lg'>Harga Crypto dalam Rupiah Hari Ini</span>
-        <SearchIcon />
+        <SearchIcon onClick={onClickSearchIcon} />
       </div>
+      {!isHidden && (
+        <div className='px-4 md:hidden absolute top-0 z-1 w-screen h-screen bg-white mt-4' />
+      )}
     </div>
   )
 };
