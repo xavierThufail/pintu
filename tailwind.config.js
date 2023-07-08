a4c9cf0033c18573060352ecb198c408667a4762
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 01:13:20 +0700

    create animation scroll fade in

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scroll: 'scrollIn 0.5s ease-in-out',
+      },
+      keyframes: () => ({
+        scrollIn: {
+          '0%': { marginTop: '-100vh', opacity: 0 },
+          '100%': { marginTop: '0', opacity: 1 },
+        },
+      }),
+    },
   },
   plugins: [],
 }
