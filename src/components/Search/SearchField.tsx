15cf86f4cdfd43227c94042f0d9605c86d7d427e
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 00:35:40 +0700

    handle onFocus event

--- src/components/Search/SearchField.tsx
+++ src/components/Search/SearchField.tsx
@@ -5,11 +5,13 @@ import { ButtonProps, onChangeInput } from '../../constants/types';
 
 export type SearchFieldProps = {
   onClose?: () => void;
+  onFocus?: () => void;
   autoFocus?: boolean;
 };
 
 export type SearchInputType = {
   autoFocus: SearchFieldProps['autoFocus'];
+  onFocus: SearchFieldProps['onFocus'];
   value: string;
   onChange: onChangeInput;
 };
@@ -26,7 +28,7 @@ const SearchIcon = () => (
   </div>
 );
 
-const SearcInput = ({ autoFocus, value, onChange }: SearchInputType) => (
+const SearcInput = ({ autoFocus, value, onChange, onFocus }: SearchInputType) => (
   <div className='flex w-full'>
     <input
       autoFocus={autoFocus}
@@ -35,11 +37,12 @@ const SearcInput = ({ autoFocus, value, onChange }: SearchInputType) => (
       placeholder='Cari aset di Pintu...'
       className='bg-transparent focus:outline-0 pl-3 min-w-full'
       onChange={onChange}
+      onFocus={onFocus}
     />
   </div>
 );
 
-const SearchField = ({ onClose, autoFocus }: SearchFieldProps) => {
+const SearchField = ({ onClose, autoFocus, onFocus }: SearchFieldProps) => {
   const [searchText, setSearchText] = React.useState<string>('');
 
   const handleInputChange: onChangeInput = ({ target: { value } }) => {
@@ -51,6 +54,7 @@ const SearchField = ({ onClose, autoFocus }: SearchFieldProps) => {
       <div className='flex items-center items-center rounded-lg p-3 bg-zinc-100'>
         <SearchIcon />
         <SearcInput
+          onFocus={onFocus}
           value={searchText}
           onChange={handleInputChange}
           autoFocus={autoFocus}
