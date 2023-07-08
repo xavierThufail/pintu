a169c95ed8deaa62f8143d3e2f14969083b23d52
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 01:03:27 +0700

    implement custom hook to provide context value

--- src/pages/Homepage/Container.tsx
+++ src/pages/Homepage/Container.tsx
@@ -1,13 +1,26 @@
+import useHomepage from './useHomepage';
 import { CurrenciesContext } from '../../hooks/useCurrencies';
 import Header from '../../components/Header';
 import SearchSection from '../../components/Search';
+import { LoadingIcon } from '../../assets';
 
 const Homepage = () => {
+  const {
+    currencies,
+    error,
+    isLoading
+  } = useHomepage();
+
+  if (error) return (
+    <span>ERROR</span>
+  )
+
   return (
-    <CurrenciesContext.Provider value={[]}>
+    <CurrenciesContext.Provider value={currencies}>
       <div className='max-w-screen-xl mx-auto'>
         <Header />
         <SearchSection />
+        {isLoading && <LoadingIcon />}
       </div>
     </CurrenciesContext.Provider>
   );
