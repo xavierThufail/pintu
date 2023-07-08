8058932bac2184aed4db96600e4dd7f8009f4625
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 01:12:08 +0700

    show top movers section to homepage

--- src/pages/Homepage/Container.tsx
+++ src/pages/Homepage/Container.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
+
 import useHomepage from './useHomepage';
 import { CurrenciesContext } from '../../hooks/useCurrencies';
 import Header from '../../components/Header';
 import SearchSection from '../../components/Search';
+import TopMovers from '../../components/TopMovers';
 import { LoadingIcon } from '../../assets';
+
+const Content = () => (
+  <React.Fragment>
+    <TopMovers />
+  </React.Fragment>
+);
 
 const Homepage = () => {
   const {
@@ -20,7 +29,7 @@ const Homepage = () => {
       <div className='max-w-screen-xl mx-auto'>
         <Header />
         <SearchSection />
-        {isLoading && <LoadingIcon />}
+        {isLoading ? <LoadingIcon /> : <Content />}
       </div>
     </CurrenciesContext.Provider>
   );
