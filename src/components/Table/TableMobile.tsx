ec426919f4326c238200d9c4de2360b59b898cc5
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 04:19:51 +0700

    wrapped component and add key

--- src/components/Table/TableMobile.tsx
+++ src/components/Table/TableMobile.tsx
@@ -9,43 +9,52 @@ import PercentageMove from '../PercentageMove';
 const SelectTableHead = ({ setPriceType }: { setPriceType: (priceType: PriceKey) => void }) => (
   <select className='border rounded p-1 text-xs' onChange={(event) => setPriceType(event.target.value as PriceKey)}>
     {PRICE_TYPE.map((type) => (
-      <option value={type.value}>{type.text}</option>
+      <option key={type.value} value={type.value}>{type.text}</option>
     ))}
   </select>
 );
 
 const TableHead = ({ setPriceType }: { setPriceType: (priceType: PriceKey) => void }) => (
-  <tr>
-    <th className='p-4 border-solid border-t w-screen text-sm font-medium'>
-      <div className='flex justify-between items-center'>
-        <span>Crypto</span>
-        <div>
-          <SelectTableHead setPriceType={setPriceType} />
-        </div>
-      </div>
-    </th>
-  </tr>
-);
-
-const TableData = ({ logo, name, currencySymbol, price, priceType }: CurrencyWithPriceType & { priceType: PriceKey }) => (
-  <tr>
-    <td className='p-4 border-solid border-t w-screen text-sm font-medium'>
-      <div className='flex justify-between items-center'>
-        <div className="flex items-center">
-          <img src={logo} height={32} width={32} className="mr-4" />
-          <div className='flex flex-col'>
-            <span className='font-bold'>{name}</span>
-            <span className='text-gray-400'>{currencySymbol}</span>
+  <thead>
+    <tr>
+      <th className='p-4 border-solid border-t w-screen text-sm font-medium'>
+        <div className='flex justify-between items-center'>
+          <span>Crypto</span>
+          <div>
+            <SelectTableHead setPriceType={setPriceType} />
           </div>
         </div>
-        <div className='flex flex-col items-end'>
-          <span className='font-bold'>{numberToCurrency(price.latestPrice)}</span>
-          <PercentageMove value={price[priceType]} />
-        </div>
-      </div>
-    </td>
-  </tr>
+      </th>
+    </tr>
+  </thead>
 );
+
+const TableData = ({ priceType, currencies }:  { priceType: PriceKey, currencies: CurrencyWithPriceType[] }) => {
+  
+  return (
+    <tbody>
+      {currencies.map(({ logo, name, currencySymbol, price }) => (
+        <tr key={currencySymbol}>
+          <td className='p-4 border-solid border-t w-screen text-sm font-medium'>
+            <div className='flex justify-between items-center'>
+              <div className="flex items-center">
+                <img src={logo} height={32} width={32} className="mr-4" />
+                <div className='flex flex-col'>
+                  <span className='font-bold'>{name}</span>
+                  <span className='text-gray-400'>{currencySymbol}</span>
+                </div>
+              </div>
+              <div className='flex flex-col items-end'>
+                <span className='font-bold'>{numberToCurrency(price.latestPrice)}</span>
+                <PercentageMove value={price[priceType]} />
+              </div>
+            </div>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
 
 const TableMobile = () => {
   const [priceType, setPriceType] = React.useState<PriceKey>('day');
@@ -55,9 +64,7 @@ const TableMobile = () => {
     <div className='mt-2 md:hidden'>
       <table>
         <TableHead setPriceType={setPriceType} />
-        {currencies.map((currency) => (
-          <TableData {...currency} priceType={priceType} />
-        ))}
+        <TableData currencies={currencies} priceType={priceType} />
       </table>
     </div>
   )
