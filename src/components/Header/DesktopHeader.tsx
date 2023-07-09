25133c4168863b9ae4e95855420f748ebe2efbfc
Xavier Thufail <xavier.thufail@traveloka.com>
2023-07-09 17:18:21 +0700

    show sub menu when hover item menu

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -7,6 +7,7 @@ import { LogoHeader, MenuSubItemContent} from './shared';
 import useOutsideClick from '../../hooks/useCheckClick';
 
 type LanguageButtonProps = ButtonProps & {
+  onMouseOut: () => void;
   active: boolean;
   item: ItemType;
 };
@@ -45,29 +46,32 @@ const MenuSubItem = ({ title, subItems, justifyEnd }: MenuSubItemProps) => (
 
 const LanguageButton = ({ onClick, active, item }: LanguageButtonProps) => (
   <React.Fragment>
-    <div className='flex items-center relative z-20' onClick={onClick}>
+    <div
+      onClick={onClick}
+      onMouseOver={onClick}
+      className='relative z-50 flex items-center relative z-20 cursor-pointer'
+    >
       <img className='pr-2' src={images.flagIndo} height={28} width={28} alt="id" />
       <span className='pr-2'>ID</span>
       <DirectionIcon height={16} width={16} direction={active ? 'DOWN' : 'UP'} />
+      {active && item.subItems.length > 0 && <MenuSubItem subItems={item.subItems} title='BAHASA' justifyEnd />}
     </div>
-    {active && item.subItems.length > 0 && <MenuSubItem subItems={item.subItems} title='BAHASA' justifyEnd />}
   </React.Fragment>
 );
 
 const MenuItem = ({ active, onClick, item, justifyEnd }: MenuItemProps) => (
-  <React.Fragment>
-    <div
-      onClick={onClick}
-      className='relative z-50 cursor-pointer'
+  <div
+    onClick={onClick}
+    onMouseEnter={onClick}
+    className='relative z-50 cursor-pointer'
+  >
+    <h1
+      className={`text px-5 lg:px-10 ${active ? 'text-gray-400' : ''}`}
     >
-      <h1
-        className={`text px-5 lg:px-10 ${active ? 'text-gray-400' : ''}`}
-      >
-        {item.title}
-      </h1>
-    </div>
+      {item.title}
+    </h1>
     {active && item.subItems.length > 0 && <MenuSubItem subItems={item.subItems} justifyEnd={justifyEnd} />}
-  </React.Fragment>
+  </div>
 );
 
 const Menu = ({ list, handleClickItem, activeId }: MenuProps) => {
@@ -78,23 +82,26 @@ const Menu = ({ list, handleClickItem, activeId }: MenuProps) => {
   });
 
   return (
-    <div ref={menuRef} className='flex items-start'>
-      {list.map((item, id) => {
-        const Component = item.title === 'Bahasa'
-          ? LanguageButton
-          : MenuItem;
+    <div className='pb-4' onMouseLeave={handleClickItem(-1)}>
+      <div ref={menuRef} className='flex items-start'>
+        {list.map((item, id) => {
+          const Component = item.title === 'Bahasa'
+            ? LanguageButton
+            : MenuItem;
 
-        return (
-          <div key={id} className='relative'>
-            <Component
-              onClick={handleClickItem(id)}
-              active={activeId === id}
-              item={item}
-              justifyEnd={activeId >= (list.length - 3)}
-            />
-          </div>
-        );
-      })}
+          return (
+            <div key={id} className='relative'>
+              <Component
+                onMouseOut={handleClickItem(-1)}
+                onClick={handleClickItem(id)}
+                active={activeId === id}
+                item={item}
+                justifyEnd={activeId >= (list.length - 3)}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
@@ -105,9 +112,11 @@ const DesktopHeader = () => {
   const handleClickItem: MenuProps['handleClickItem'] = (id) => () => setActiveId(id);
 
   return (
-    <div className='p-6 pt-8 hidden md:block'>
+    <div className='px-4 pt-8 hidden md:block'>
       <div className='flex justify-between'>
-        <LogoHeader href='/' />
+        <div className='pb-2'>
+          <LogoHeader href='/' />
+        </div>
         <Menu list={listMenu} handleClickItem={handleClickItem} activeId={activeId} />
       </div>
     </div>
